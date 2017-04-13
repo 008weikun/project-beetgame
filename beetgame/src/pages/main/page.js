49ba254e75cd7021f88b239e6f0e8c86867627a8
Brave <u9648u6653u52c7@gmail.com>
2017-04-13 16:27:39 +0800

    增加页面主框架

---
+++ beetgame/src/pages/main/page.js
@@ -0,0 +1,5 @@
+require('./page.less');
+require('components/sui/index');
+require('components/ui/header/index').render();
+require('components/ui/panel/index').render();
+require('components/ui/index/index').render();

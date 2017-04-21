143b58736adee12562d88bac4aed0f09fa47459a
Brave <u9648u6653u52c7@gmail.com>
2017-04-21 16:53:48 +0800

    比赛规则

---
+++ beetgame/src/components/ui/game/p3/index.js
@@ -0,0 +1,19 @@
+require('./index.less');
+var $ = require('zepto');
+var _ = require('lodash');
+var url = require('url-lib');
+var params = url.parseQuery(location.href);
+var template = require('./index.hbs');
+
+var root = $('#J_content .swiper-slide').eq(2);
+var data = {
+    award: _.isEmpty(params.award) ? '' : params.award
+};
+
+function render() {
+    root.append(template(data));
+}
+
+module.exports = {
+    render: render
+};
